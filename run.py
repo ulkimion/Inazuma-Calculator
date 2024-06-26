11bb448e2acdf296938d9bf462c643bbcd91d628
ulkimion <101999652+ulkimion@users.noreply.github.com>
2024-06-26 17:52:16 -0400

    version 1
    a

---
+++ run.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import json
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    with open("database/VRoadDB.json", encoding="utf-8") as file:
+        data=json.load(file)
+    return render_template("index.html", players=data)
+
+@app.route("/p/<string:slug>/")
+def show_post(slug):
+    return render_template("post_view.html", slug_title=slug)
+
+@app.route("/admin/post/")
+@app.route("/admin/post/<int:post_id>")
+def post_form(post_id=None):
+    return render_template("admin/post_form.html", post_id=post_id)
+
+@app.route("/signup/", methods=["GET", "POST"])
+def show_signup_form():
+    return render_template("signup_form.html")
