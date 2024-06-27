34a916e9ac66d832de05418e6d2f8b0668473051
ulkimion <101999652+ulkimion@users.noreply.github.com>
2024-06-27 18:22:34 -0400

    fin avances hoy, trabajando paginas personajes

--- run.py
+++ run.py
@@ -14,11 +14,8 @@ def index():
 def show_post(slug):
     return render_template("post_view.html", slug_title=slug)
 
-@app.route("/admin/post/")
-@app.route("/admin/post/<int:post_id>")
-def post_form(post_id=None):
-    return render_template("admin/post_form.html", post_id=post_id)
-
-@app.route("/signup/", methods=["GET", "POST"])
-def show_signup_form():
-    return render_template("signup_form.html")
+@app.route("/player/<string:slug>/")
+def player_data(slug):
+    with open("database/VRoadDB.json", encoding="utf-8") as file:
+        data=json.load(file)
+    return render_template("player_data.html", player=data, slug_title=slug)
